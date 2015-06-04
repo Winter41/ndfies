585dcc63305016dbd655a75b7491b26220d57366
John Nico Garcia <imOusy414@gmail.com>
2015-06-04 18:07:32 +0800

    Adds player preview

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require bootstrap-sprockets
 //= require toastr.min
-//= require jquery.uploadPreview.min
+//= require jquery.uploadPreview
 //= require jquery_ujs
 //= require jquery-fileupload/basic
 //= require jquery.jplayer
