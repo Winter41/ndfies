672737cf450e7d4e44a652176660b5ac1b1c9527
John Nico Garcia <imOusy414@gmail.com>
2015-06-17 11:45:20 +0800

    YEAH

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150527044313) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "albums", force: :cascade do |t|
     t.string   "name"
     t.integer  "artist_id"
@@ -20,7 +23,7 @@ ActiveRecord::Schema.define(version: 20150527044313) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "albums", ["artist_id"], name: "index_albums_on_artist_id"
+  add_index "albums", ["artist_id"], name: "index_albums_on_artist_id", using: :btree
 
   create_table "artists", force: :cascade do |t|
     t.string   "name"
@@ -36,7 +39,7 @@ ActiveRecord::Schema.define(version: 20150527044313) do
     t.string   "file"
   end
 
-  add_index "tracks", ["album_id"], name: "index_tracks_on_album_id"
+  add_index "tracks", ["album_id"], name: "index_tracks_on_album_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -53,7 +56,9 @@ ActiveRecord::Schema.define(version: 20150527044313) do
     t.datetime "updated_at"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "albums", "artists"
+  add_foreign_key "tracks", "albums"
 end
